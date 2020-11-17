0ffe2da57e8685ad46a32d952828865877c3a4f4
amanda-dale <71297649+amanda-dale@users.noreply.github.com>
2020-11-16 19:47:27 -0500

    Update CustomerMenuHandler.java

--- NotSoloClassProject/src/main/java/com/notsolo/notsoloclassproject/CustomerMenuHandler.java
+++ NotSoloClassProject/src/main/java/com/notsolo/notsoloclassproject/CustomerMenuHandler.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package com.notsolo.notsoloclassproject;
+import java.util.Scanner;
 
 /**
  *
@@ -18,6 +19,58 @@ public class CustomerMenuHandler {
     
     public void InRoute()
     {
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while(loop) {
+            System.out.println("Would you like to:"+"\n"+"(1)Get menu"+"\n"+"(2)Place an Order"+"\n"+"(3)Remove an Order"+"\n"+"(4)See Orders"+"\n"+"(5)Get Reciept"+"\n"+"(6)Cash Out"+"\n"+"(7)Log out temporarily");
+            int choice = scanner.nextInt();
+            while(!scanner.hasNextInt() || choice > 7){
+                System.out.println("Please enter a valid option (1 through 7)");
+        }
+            if(choice > 0 && choice < 7){
+                switch(choice){
+                    case 1:
+                        GetMenu();
+                        break;
+                    case 2:
+                        PlaceOrder();
+                        break;
+                    case 3:
+                        RemoveOrder();
+                        break;
+                    case 4:
+                        SeeOrders();
+                        break;
+                    case 5:
+                        GetReciept();
+                        break;
+                    case 6:
+                        CashOut();
+                        break;
+                    default:
+                        System.out.println("Thank you for using our program");
+                        loop = false;
+                        break;
+                }
+            }
+        }
+    }
+    private static void GetMenu(){
+        
+    }
+    private static void PlaceOrder(){
+        
+    }
+    private static void RemoveOrder(){
+        
+    }
+    private static void SeeOrders(){
+        
+    }
+    private static void GetReciept(){
+        
+    }
+    private static void CashOut(){
         
     }
 }
